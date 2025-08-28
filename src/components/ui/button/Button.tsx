5b64e04f066a6d79a6e679641710382794a75867
rusya <almostz@yandex.ru>
2025-08-28 18:48:13 +0300

    💄 Add shadcn/button

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,42 @@
+import type { PrimitiveProps } from "reka-ui"
+import { defineComponent, type HTMLAttributes } from "vue"
+import type { ButtonVariants } from "."
+import { Primitive } from "reka-ui"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "."
+import { prop } from "@/lib/prop"
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants["variant"]
+  size?: ButtonVariants["size"]
+  class?: HTMLAttributes["class"]
+  whenClick?: HTMLAttributes['onClick']
+}
+
+const props = {
+  variant: prop<Props['variant']>().optional(),
+  size: prop<Props['size']>().optional(),
+  class: prop<Props['class']>().optional(),
+  as: prop<Props['as']>().optional('button'),
+  asChild: prop<Props['asChild']>().optional(),
+  whenClick: prop<Props['whenClick']>().optional(),
+}
+
+export const Button = defineComponent({
+  name: 'Button',
+  props,
+  setup(props, { slots }) {
+    return () => (
+      <Primitive
+        data-slot="button"
+        as={props.as}
+        asChild={props.asChild}
+        class={cn(buttonVariants({ variant: props.variant, size: props.size }), props.class)}
+        {...{onclick: props.whenClick}}
+
+      >
+        {slots.default?.()}
+      </Primitive>
+    )
+  }
+})
