69e9bee2912698567203f60e93e3d30a2279187a
rusya <almostz@yandex.ru>
2025-09-14 16:20:09 +0300

    🍱 Add ozon tech logo

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -10,18 +10,30 @@ export const DefaultLayout = defineComponent({
     const dark = useDark()
     const toggleTheme = useToggle(dark)
 
+    const renderLogo = () => (
+      <div class='flex flex-row'>
+        <img src='/images/tech.svg' class={'h-8'}/>
+        <div class='py-1 flex flex-col'>
+          <img src='/images/ozon.svg' class={'h-2'}/>
+          <span class='font-bold text-xs'>&nbsp;seq-ui</span>
+        </div>
+      </div>
+    )
+
     return () => (
-      <div class="w-full h-[100dvh] flex flex-col gap-[20px] p-[20px]">
-        <div class="flex justify-between items-center border-1 rounded-md p-[4px] pl-[12px]">
-          <RouterLink to="/">SeqUI</RouterLink>
+      <div>
+        <div class="flex justify-between items-center border-1 p-[4px] pl-[12px]">
+          {renderLogo()}
           <Button
             variant={'ghost'}
             size={'icon'}
             whenClick={() => toggleTheme()}
             >{ dark.value ? <SunMedium/> : <MoonIcon/> }
           </Button>
-          </div>
-        { slots.default?.() }
+        </div>
+        <div class="w-full flex flex-col gap-[20px] p-[20px]">
+          { slots.default?.() }
+        </div>
       </div>
     )
   }
